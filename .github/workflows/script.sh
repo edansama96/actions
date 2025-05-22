f3ba3544d556aed531a232f0d29805c191215483
Edwin Sánchez <edwin.196@outlook.com>
2025-05-22 11:49:59 -0500

    instalación y verificación de node

--- .github/workflows/script.sh
+++
@@ -1,6 +0,0 @@
-#sudo para dar permisos de admin y comando para actualizar paquqetes nueva prueba
-sudo apt-get update
-# sudo permisos de admin y comando para instalar curl
-#como no se tiene una interación en el proceso de usa la badenda -y para
-#instalar sin pedir confirmación
-sudo apt-get install curl -y
