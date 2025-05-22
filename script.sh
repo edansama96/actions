27c64b2a2d306bd1f8983b0fb9799b3c7c0aa086
Edwin Sánchez <edwin.196@outlook.com>
2025-05-22 11:24:51 -0500

    problemas por la ubicación del archivo script que se utiliza ya se ubico en el espacio correcto

---
+++ script.sh
@@ -0,0 +1,6 @@
+#sudo para dar permisos de admin y comando para actualizar paquqetes
+sudo apyt-get update
+# sudo permisos de admin y comando para instalar curl
+#como no se tiene una interación en el proceso de usa la badenda -y para
+#instalar sin pedir confirmación
+sudo apt-get install curl -y
